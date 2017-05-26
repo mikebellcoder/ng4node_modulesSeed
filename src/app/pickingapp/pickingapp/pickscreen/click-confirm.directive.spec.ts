32ba059dfbcb7f7f8bca0cdb04b1e4a2352088b2
ngMike <mikebell@scientology.net>
2017-05-26 12:03:25 -0700

    cleanup

--- src/app/pickingapp/pickingapp/pickscreen/click-confirm.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ClickConfirmDirective } from './click-confirm.directive';
-
-describe('ClickConfirmDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ClickConfirmDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  nserver: `http://10.16.45.17:3003`
-};
